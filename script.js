5251e296032f40259d61bc17e8e1f19e4ade440b
kaguhe <107109714+therken@users.noreply.github.com>
2023-03-16 07:57:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ $(document).ready(function() {
       this.style.height = (this.scrollHeight) + 'px';
     });
   });
+  $(document).ready(function() {
+    $('#profile-photo').change(function() {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $('label[for="profile-photo"] img').attr('src', e.target.result);
+      };
+      reader.readAsDataURL(this.files[0]);
+    });
+  });
