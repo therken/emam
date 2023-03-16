ed4e8e34c8022678ee4918b3f406d28c0ddc5264
kaguhe <107109714+therken@users.noreply.github.com>
2023-03-16 07:57:25 +0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -2,15 +2,14 @@
 $host= 'localhost';
 $user = 'root';
 $pass = '';
-$db = 'forma';
-  
+$db = 'form'; 
 $conn = mysqli_connect($host , $user , $pass , $db);
 // Check connection
 if($conn === false){
-  die("ERROR: Could not connect. "
+  die("ERROR: Невозможно подключиться. "
       . mysqli_connect_error());
 }
-
+///переменные
 $name = mysqli_real_escape_string($conn ,$_POST['name']);
 $sec = mysqli_real_escape_string($conn,$_POST['secondname']);
 $last = mysqli_real_escape_string($conn,$_POST['lastname']);
@@ -18,21 +17,61 @@ $mail = mysqli_real_escape_string($conn,$_POST['mail']);
 $about = mysqli_real_escape_string($conn,$_POST['about']);
 $edate=strtotime($_POST['date']); 
 $edate=date("Y-m-d",$edate);
+$startDate = date('Y-m-d', strtotime("01/01/1900"));
+$endDate = date('Y-m-d', strtotime("01/10/2024"));
 
-$folder ="./uploads/";
-$file_ext =  strtolower(strrchr($_FILES['image']['name'],'.'));
-$file_name = "file".uniqid(rand(10000,99999));
-move_uploaded_file($tmp_name, "$uploads_dir/$name")
-$uploadedFile  = $folder.$file_name.$file_ext;
-$sql = "INSERT INTO user_form (firstname, secondname, lastname, mail, about, dendata,img)
-VALUES ('$name', '$sec', '$last', '$mail', '$about', '$edate','$file_name')";
+///проверка существования email-адреса
+$sql = "SELECT mail FROM user_form WHERE mail='$mail'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$errorMessage = "Этот email уже используется";
+echo "$errorMessage";
+} else {
+  ///проверка даты
+if (($edate >= $startDate) && ($edate <= $endDate)) {
+  echo "";
+} else {
+  $errorMessage = "некорректная дата";
+  echo "$errorMessage"; 
 
-if(mysqli_query($conn, $sql)){
-  echo "<h3>Информация добавлена.</h3>";  
-} else{
-  echo "Ошибка $sql. "
-      . mysqli_error($conn);
+}
+///загрузка файла 
+$uploaddir = 'uploads/';
+$tempFilePath = $_FILES['image']['tmp_name']; 
+$fileName = $_FILES['image']['name']; 
+$uploadFile = $uploaddir . uniqid() . '-' . basename($fileName); 
+
+if(!is_uploaded_file($_FILES['image']['tmp_name'])) {
+     $errorMessage = "Выберите изображение";
+     echo "$errorMessage";
+} 
+
+//Проверка что это картинка
+if (!getimagesize($_FILES["image"]["tmp_name"])) {
+     $errorMessage = "Это не картинка...";
+     echo "$errorMessage";
 }
 
+if (move_uploaded_file($tempFilePath, $uploadFile)) {
+    $successMessage = "Файл корректен и был успешно загружен.";
+    echo "$successMessage";
+} else {
+    $errorMessage = "Возможная атака с помощью файловой загрузки!";
+    echo "'$errorMessage";
+}
+$photo_link = $uploadFile; 
+///добавление значений в бд
+  $sql = "INSERT INTO user_form (firstname, secondname, lastname, mail, about, dendata,img)
+  VALUES ('$name', '$sec', '$last', '$mail', '$about', '$edate','$photo_link')";
+  ///вывод сообщения о том что данные добавлены
+  if(mysqli_query($conn, $sql)){
+    $successMessage= "Информация добавлена.";  
+    echo "$successMessage";
+  } else{
+    $errorMessage= "Ошибка $sql. "
+        . mysqli_error($conn);
+        echo "$errormeMessage";
+  }
+}
 mysqli_close($conn);
 ?>
